41ebe54780d7261b82250e90f00e70f1e3ee748a
austinmao06 <austinmao06@gmail.com>
2026-08-12 09:20:35 -0400

    Add note on fitting values for normalized simulation
    Added additional note regarding fitting values for kNorm and wNorm.

--- normalized/pocket_functions/convert_to_tau_time.py
+++ normalized/pocket_functions/convert_to_tau_time.py
@@ -4,12 +4,10 @@ def convert_to_tau_time(true_time):
     return w * true_time
 
 #Usage
-
-
 ti = np.argmin(np.abs(tau - convert_to_tau_time(127e-9))) #finds nearest tau index to 127ns true time
-
 plt.plot(zeta, b[ti,]**2)
 
+#Additional Note
 """
 Note that the prominent issue with normalized as a whole is that one must calculate fitting values for kNorm, wNorm, etc prior to running the simulation
 to ensure that the necessary z and t dimensions are even met
