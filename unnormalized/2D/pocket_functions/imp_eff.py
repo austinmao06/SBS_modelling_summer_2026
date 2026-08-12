2164bfa3100dd5c3b8c484d041d7b4eda0fad548
austinmao06 <austinmao06@gmail.com>
2026-08-12 14:35:05 -0400

    Create imp_eff.py

---
+++ unnormalized/2D/pocket_functions/imp_eff.py
@@ -0,0 +1,11 @@
+#Implicit efficiency estimates efficiency with reasonable accuracy at late times
+
+def get_eff(a_in_col = a_in_col, a_out_col = a_out_col, rad=rad, tau=tau):
+    P_in  = np.trapezoid(a_in_col**2  * rad[None, :], rad, axis=1)   # pump power entering
+    P_out = np.trapezoid(a_out_col**2 * rad[None, :], rad, axis=1)   # pump power leaving
+    cumtrap = lambda y: np.concatenate(([0.0], np.cumsum(0.5*(y[1:]+y[:-1])*np.diff(tau))))
+    supplied = cumtrap(P_in)
+    absorbed = cumtrap(P_in - P_out)
+    eta = np.divide(absorbed, supplied, out=np.full_like(tau, np.nan, dtype=float), where=(supplied > 0))
+    eta[0] = np.nan
+    return eta
